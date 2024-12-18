88068be2ad064a0652990437f9efbe57a7cdd915
radiantgruzmother <162915@gmail.com>
2024-12-18 13:19:48 -0500

    i hate this

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('hello/', include('hello.urls')),
     path('autos/', include('autos.urls')),
-    path('cats/', include('cats.urls')),
+    #path('cats/', include('cats.urls')),
     path("accounts/", include("django.contrib.auth.urls")),    
     re_path(r'^site/(?P<path>.*)$', serve,
         {'document_root': SITE_ROOT, 'show_indexes': True},
